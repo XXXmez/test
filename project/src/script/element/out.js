28f6d17465b686fb06ff4021b76cf119bf62e151
Ruslan Bogdanov <russian.bogd@gmail.com>
2022-09-05 22:28:33 +0300

    init: added selection of selected user data

--- project/src/script/element/out.js
+++ project/src/script/element/out.js
@@ -4,12 +4,22 @@ class Out extends Component {
     constructor(root, firstName, lastName, description, address) {
         super(root, 'div', ['out__info']);
         
-        this.user = new Component(this.component, 'div', ['out__user'], `Выбран пользователь: ${firstName} ${lastName}`);
-        this.user = new Component(this.component, 'div', ['out__description'], `Описание: ${description}`);
-        this.user = new Component(this.component, 'div', ['out__address'], `Адрес проживания: ${address.streetAddress}`);
-        this.user = new Component(this.component, 'div', ['out__city'], `Город: ${address.city}`);
-        this.user = new Component(this.component, 'div', ['out__shtat'], `Провинция/штат: ${address.state}`);
-        this.user = new Component(this.component, 'div', ['out__index'], `Индекс: ${address.zip}`);
+        this.user = new Component(this.component, 'div', ['out__user']);
+        this.user.component.innerHTML = `Выбран пользователь: ${'<b>'}${firstName} ${lastName}${'</b>'}`;
+
+        this.description = new Component(this.component, 'div', ['out__description'], `Описание: ${description}`);
+
+        this.address = new Component(this.component, 'div', ['out__address']);
+        this.address.component.innerHTML = `Адрес проживания: ${'<b>'}${address.streetAddress}${'</b>'}`;
+
+        this.city = new Component(this.component, 'div', ['out__city']);
+        this.city.component.innerHTML = `Город: ${'<b>'}${address.city}${'</b>'}`;
+
+        this.shtat = new Component(this.component, 'div', ['out__shtat']);
+        this.shtat.component.innerHTML = `Провинция/штат: ${'<b>'}${address.state}${'</b>'}`;
+
+        this.index = new Component(this.component, 'div', ['out__index']);
+        this.index.component.innerHTML = `Индекс: ${'<b>'}${address.zip}${'</b>'}`;
     }
 }
 
